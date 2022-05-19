ca4fd141b12278b02ae971d4e36ce938fd528d24
RekiDunois <rekidunois+github@gmail.com>
2022-05-19 18:19:42 +0800

    init: change time format and color

--- init.fish
+++ init.fish
@@ -1,6 +1,9 @@
 # theme setting
+
 omf theme kawasaki
 
 set -g theme_display_time yes
 set -g theme_display_group no
 set -g theme_display_git yes
+set -g theme_color_time 33CCFF
+set -g theme_display_time_format '+%H:%M'
